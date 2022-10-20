f18abc3c7da01c43f71ebd7f961b4f4bc0de2e12
kgm52 <kgm52@drexel.edu>
2022-10-20 06:07:32 -0400

    everything

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.from("photo1", {
+    duration: 1,
+    x: 400
+});
