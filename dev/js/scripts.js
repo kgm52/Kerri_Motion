9800e4035c12eb6b3ccab472b074dda68b796768
kgm52 <kgm52@drexel.edu>
2022-11-06 18:01:30 -0500

    timeline

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,21 +5,31 @@ import { GSDevTools } from "gsap/GSDevTools";
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
-gsap.to("#line", {duration:1, drawSVG:"true"});
 
 
-// gsap.to("#small-circle", {duration:1, scale:2, x:350});
-// gsap.to("#large-circle", {duration:1, scale:0.5, x:-350});
+function simpleAnimation(){
+    var tl = gsap.timeline();
+    tl.to("#line", {duration:1, drawSVG:"true"})
+    .to("#small-circle", {duration:1, scale:2, x:350})
+    .to("#large-circle", {duration:1, scale:0.5, x:-350});
+    return tl;
+}
 
 
-// gsap.from(".square",{duration:0.5, drawSVG:0, stagger:0.25, transformOrigin:"center"},"playPattern")
+function patternAnimation(){
+    var tl = gsap.timeline();
+    tl.from(".square",{duration:0.5, drawSVG:0, stagger:0.25, transformOrigin:"center"},"playPattern");
+    return tl;
+}
 
 
-// function toggleAnimation(){
-//     var tl = gsap.timeline();
-//     tl.to("#happy-face", {duration:1, x:-100});
-//     return tl;
-// }
+function toggleAnimation(){
+    var tl = gsap.timeline();
+    tl.to("#happy-face", {duration:1, x:-100});
+    return tl;
+}
 
-// var mainTl = gsap.timeline();
-// mainTl.add(toggleAnimation());
+var mainTl = gsap.timeline();
+mainTl.add(toggleAnimation())
+.add(simpleAnimation())
+.add(patternAnimation());
