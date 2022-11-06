2b6faa0239b5bb6286ece58e2fa12ef1728166dd
kgm52 <kgm52@drexel.edu>
2022-11-06 17:17:46 -0500

    ugh

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,6 +12,8 @@ gsap.to("#line", {duration:1, drawSVG:"true"});
 // gsap.to("#large-circle", {duration:1, scale:0.5, x:-350});
 
 
+// gsap.from(".square",{duration:0.5, drawSVG:0, stagger:0.25, transformOrigin:"center"},"playPattern")
+
 
 // function toggleAnimation(){
 //     var tl = gsap.timeline();
