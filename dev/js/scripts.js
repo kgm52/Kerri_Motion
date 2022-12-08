2fca8e47ed52a922e5d1be0ef19fa0bcf9ae9d0e
kgm52 <kgm52@drexel.edu>
2022-12-08 13:22:14 -0500

    rip

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -15,18 +15,17 @@ gsap.set("#colorfill-black", {xPercent:-100, yPercent:-100});
 
 function ballAnimation(){
     var tl = gsap.timeline();
-    tl.fromTo("#ball", {scale:0}, {ease: "power2.inOut", duration:1, scale:1 })
-    .to("#ball", {x:0, y:600})
-    .fromTo("#ball", {scaleY:0}, {ease: "power2.inOut", duration:0.5, scaleY:1.5}, "-=1")
-    .to("#ball", {x:0, y:0}, "-=1")
-    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5}, "-=1.5")
-    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.5, scaleY:1.5}, "-=1.75")
-    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:1}, "-=2.25")
-    tl.fromTo("#ball", {scaleY:1}, {ease: "power2.inOut", duration:0.25, scaleY:1.5}, "-=2.5")
-    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5}, "-=2.75")
-    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.25, scaleY:1}, "-=3")
-    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5}, "-=3.25")
-    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.25, scaleY:1, scaleX:0.3}, "-=3.5")
+    tl.fromTo("#ball", {scale:0}, {ease: "power2.inOut", duration:1, scale:1, transformOrigin: "center", x:0, y:-80})
+    .fromTo("#ball", {scaleY:1}, {ease: "power2.inOut", duration:0.5, scaleY:1.5, transformOrigin: "center", x:0, y:0})
+    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5, transformOrigin: "center bottom"})
+    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.5, scaleY:1.5, transformOrigin: "center bottom", x:0, y:-80})
+    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5, transformOrigin: "center bottom", x:0, y:-20})
+    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.25, scaleY:1.5, transformOrigin: "center bottom", x:25, y:-50})
+    .fromTo("#ball", {scaleY:1.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5, transformOrigin: "center bottom", x:50, y:-25})
+    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.25, scaleY:1, transformOrigin: "center bottom", x:75, y:-30})
+    .fromTo("#ball", {scaleY:1}, {ease: "power2.inOut", duration:0.25, scaleY:0.5, transformOrigin: "center bottom", x:107, y:5})
+    .fromTo("#ball", {scaleY:0.5}, {ease: "power2.inOut", duration:0.25, scaleY:0.5, scaleX:0.1, transformOrigin: "center bottom"})
+    .from("#ball", {duration: 0.1, drawSVG: "0%"})
 
     return tl;
 }
@@ -34,24 +33,41 @@ function ballAnimation(){
 function logoAnimation(){
     var tl = gsap.timeline();
 
-    tl.fromTo("#s-white", {x:10, y:80}, {duration:0.35, x:0, y:0})
-    .fromTo("#i1-white", {y:80, rotate: -30}, {duration:0.4, y:0, rotate:0})
-    .fromTo("#c-white", {x:-10, y:80, rotate: 180}, {duration:0.6, x:0, y:0, rotate:-180})
-    .fromTo("#i2-white", {y:0}, {duration:0.75, y:0})
-    .fromTo("#l-white", {x:-15, y:60}, {duration:0.35, x:0, y:0, rotate:180})
-    .fromTo("#i3-white", {x:-15, y:80, rotate: -30}, {duration:0.5,x:0,  y:0, rotate:0})
-    .fromTo("#a-white", {x:-20, y:80, rotate: -30}, {duration:0.55, x:0, y:0, rotate:180})
-    .fromTo("#n1-white", {x:-15, y:80, rotate: 0}, {duration:0.4, x:0, y:80, rotate:-180})
-    .fromTo("#d-white", {x:10, y:60, rotate: 30}, {duration:0.3, x:0, y:0, rotate:0})
-    
-    .fromTo("#o-white", {y:60, rotate: -30}, {duration:0.35, y:80, rotate:0})
-    .fromTo("#n2-white", {y:60, rotate: -30}, {duration:0.3, y:80, rotate:0})
-    .fromTo("#k-white", {y:60, rotate: -30}, {duration:0.25, y:80, rotate:0})
-    .fromTo("#e-white", {y:60, rotate: -30}, {duration:0.5, y:80, rotate:0})
-    .fromTo("#y-white", {y:60, rotate: -30}, {duration:0.25, y:80, rotate:0})
+    tl.fromTo("#logo-white", {opacity:0}, {duration:0.1, opacity:1})
+    .fromTo("#s-white", {x:10, y:-80}, {duration:0.35, x:0, y:0, drawSVG: "100%"})
+    .fromTo("#i1-white", {y:-80, rotate: -30}, {duration:0.4, y:0, rotate:0, drawSVG: "100%"}, "<")
+    .fromTo("#c-white", {x:-10, y:-80, rotate: 0}, {duration:0.6, x:0, y:0, rotate:-180, drawSVG: "100%"}, "<")
+    .fromTo("#i2-white", {y:0}, {duration:0.75, y:0, drawSVG: "100%"}, "<")
+    .fromTo("#l-white", {x:-15, y:-60, rotate: 0}, {duration:0.35, x:0, y:0, rotate:180, drawSVG: "100%"}, "<")
+    .fromTo("#i3-white", {x:-15, y:-80, rotate: -30}, {duration:0.5,x:0,  y:0, rotate:0, drawSVG: "100%"}, "<")
+    .fromTo("#a-white", {x:-20, y:-80, rotate: 0}, {duration:0.55, x:0, y:0, rotate:180, drawSVG: "100%"}, "<")
+    .fromTo("#n1-white", {x:-15, y:-80, rotate: 0}, {duration:0.4, x:0, y:80, rotate:-180, drawSVG: "100%"}, "<")
+    .fromTo("#d-white", {x:10, y:-60, rotate: 30}, {duration:0.3, x:0, y:0, rotate:0, drawSVG: "100%"}, "<")
+    .fromTo("#o-white", {x:10, y:10, rotate: -30}, {duration:0.35, x:0, y:0, rotate:0, drawSVG: "100%"}, "<")
+    .fromTo("#n2-white", {y:60, rotate: 0}, {duration:0.3, y:0, rotate:180, drawSVG: "100%"}, "<")
+    .fromTo("#k-white", {x:10, y:-10, rotate: 20}, {duration:0.25, x:0, y:0, rotate:0, drawSVG: "100%"}, "<")
+    .fromTo("#e-white", {x:-60, y:-60, rotate: -45}, {duration:0.5, x:0, y:0, rotate:0, drawSVG: "100%"}, "<")
+    .fromTo("#y-white", {x:-40, y:-40, rotate: -30}, {duration:0.25, x:0, y:0, rotate:0, drawSVG: "100%"}, "<")
+
+    .fromTo("#logo-black", {opacity:0}, {duration:0.1, opacity:1}, "<")
+    .fromTo("#s-black", {x:5, y:-40}, {duration:0.35, x:0, y:0}, "<")
+    .fromTo("#i1-black", {x:0, y:-40, rotate: -30}, {duration:0.4, x:0, y:0, rotate:0}, "<")
+    .fromTo("#c-black", {x:-5, y:-40, rotate: 0}, {duration:0.6, x:0, y:0, rotate:0}, "<")
+    .fromTo("#i2-black", {x:0, y:0}, {duration:0.75, x:0, y:0}, "<")
+    .fromTo("#l-black", {x:-10, y:-30, rotate: 0}, {duration:0.35, x:0, y:0, rotate:0}, "<")
+    .fromTo("#i3-black", {x:-15, y:-40, rotate: -30}, {duration:0.5,x:0,  y:0, rotate:0}, "<")
+    .fromTo("#a-black", {x:-10, y:-40, rotate: 0}, {duration:0.55, x:0, y:0, rotate:0}, "<")
+    .fromTo("#n1-black", {x:-10, y:-40, rotate: 0}, {duration:0.4, x:0, y:80, rotate:-0}, "<")
+    .fromTo("#d-black", {x:5, y:-30, rotate: 30}, {duration:0.3, x:0, y:0, rotate:0}, "<")
+    .fromTo("#o-black", {x:5, y:15, rotate: -30}, {duration:0.35, x:0, y:0, rotate:0}, "<")
+    .fromTo("#n2-black", {x:0, y:-30, rotate: 0}, {duration:0.3, x:0, y:0, rotate:180}, "<")
+    .fromTo("#k-black", {x:15, y:-5, rotate: 20}, {duration:0.25, x:0, y:0, rotate:0}, "<")
+    .fromTo("#e-black", {x:-30, y:-30, rotate: -45}, {duration:0.5, x:0, y:0, rotate:0}, "<")
+    .fromTo("#y-black", {x:-20, y:-20, rotate: -30}, {duration:0.25, x:0, y:0, rotate:0}, "<")
 
     return tl;
 }
+
 
 function colorfillAnimation(){
     var tl = gsap.timeline();
@@ -64,7 +80,7 @@ function colorfillAnimation(){
 
 var mainTL = gsap.timeline();
 mainTL.add(ballAnimation())
-.add(logoAnimation(), "-=0.75")
+.add(logoAnimation(), "-=1.65")
 .add(colorfillAnimation(), "-=0.25")
 
 GSDevTools.create();
