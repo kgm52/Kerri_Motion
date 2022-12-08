311fc49b73d82abfcd3a44886d23ab6806271779
kgm52 <kgm52@drexel.edu>
2022-12-08 06:08:56 -0500

    stuff

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -4,3 +4,14 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
+
+
+function ballAnimation(){
+    var tl = gsap.timeline();
+    tl.to("#ball" {duration:0.5, y:"400"})
+}
+
+var mainTL = gsap.timeline();
+mainTL.add(ballAnimation())
+
+GSDevTools.create();
